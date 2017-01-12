04562fc0e3a3d021bcdfe6b25754acf518d8a5ad
Nick Coutsos <nick@coutsos.com>
2017-01-11 23:06:42 -0500

    Update demo defaults to fit the editor window more easily

--- demo/default-scene.js
+++ demo/default-scene.js
@@ -1,15 +1,23 @@
-let base = new three.Mesh(new three.PlaneGeometry(40, 40));
+let base = new three.Mesh(
+  new three.PlaneGeometry(40, 40)
+);
 base.userData.className = 'floor';
 base.rotation.x = -Math.PI/2;
 base.receiveShadow = true;
 
-let sphere = new three.Mesh(new three.SphereGeometry(5, 20, 20));
+let sphere = new three.Mesh(
+  new three.SphereGeometry(5, 20, 20)
+);
 sphere.userData.className = 'dull';
 sphere.castShadow = true;
 sphere.position.set(-5, 5, 0);
-sphere.onBeforeRender = () => sphere.rotation.y += 0.005;
+sphere.onBeforeRender = () => {
+  sphere.rotation.y += 0.005;
+}
 
-let box = new three.Mesh(new three.BoxGeometry(5, 5, 5));
+let box = new three.Mesh(
+  new three.BoxGeometry(5, 5, 5)
+);
 box.userData.className = 'ghost'
 box.userData.ticks = 0;
 box.position.set(5, 5, -2);
@@ -19,18 +27,23 @@ box.onBeforeRender = () => {
   box.rotation.y += 0.005;
   if (box.userData.ticks > 1) {
     box.userData.ticks = 0;
-    box.userData.className = box.userData.className.indexOf('selected') === -1
-      ? 'ghost selected'
-      : 'ghost';
+    box.userData.className =
+      box.userData.className.indexOf('selected') === -1
+        ? 'ghost selected'
+        : 'ghost';
   }
 }
 
-let ring = new three.Mesh(new three.TorusGeometry(4, 1, 8, 20));
+let ring = new three.Mesh(
+  new three.TorusGeometry(4, 1, 8, 20)
+);
 ring.userData.className = 'matrix';
 ring.position.set(2, 2, 7);
 ring.rotation.x = Math.PI / 2;
 ring.castShadow = true;
-ring.onBeforeRender = () => ring.rotation.z -= 0.005;
+ring.onBeforeRender = () => {
+  ring.rotation.z -= 0.005;
+}
 
 let light = new three.PointLight();
 light.position.set(5, 15, 0);
